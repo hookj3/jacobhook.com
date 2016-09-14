c0bfbfdf18862cc066d598b08d8eea5c35047d58
jhook <jhook@srspharmacy.com>
2016-09-13 21:47:53 -0400

    navigation and images

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-hello, world.
+<html>
+<head>
+	<title>Jacob Hook</title>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"></link>
+	<link href="https://fonts.googleapis.com/css?family=Cardo" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+</head>
+<style type="text/css">
+
+</style>
+<body>
+	<div class="col-md-12">
+		<div class="spacer"></div>
+		<div class="col-md-4 col-md-offset-4">
+			<h1 id="title">Jacob Hook</h1>
+			<div class="col-md-12">
+				<div id="navigation">
+					<div class="links"><a href="work.php">Work</a> | <a href="blog.php">Blog</a> | <a href="contact.php"> Contact</a></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
